c2c41b0f7fcd33fdf43be9ecf233f67357898524
SB1-dev <sadokchameseddine.barbouche@insat.ucar.tn>
2024-10-19 08:43:08 +0100

    Speaker issue fixed

--- voice_assistant/src/text_to_speech.py
+++ voice_assistant/src/text_to_speech.py
@@ -4,7 +4,7 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 
 for voice in voices:
-    if 'fr' in voice.languages[0]:
+    if hasattr(voice, 'languages') and len(voice.languages) > 0 and 'fr' in voice.languages[0]:
         engine.setProperty('voice', voice.id)
         break
 
@@ -13,3 +13,4 @@ engine.setProperty('rate', 170)
 def text_to_speech(text):
     engine.say(text)
     engine.runAndWait()
+
